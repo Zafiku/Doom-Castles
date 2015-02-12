8cf2be426ab437d8ffbf1f9cee0a49fed0f23926
Diego <diegotorresortiz@hotmail.es>
2015-02-12 19:53:23 +0100

    Scrum_01_1

---
+++ Assets/Scripts/Player_disparo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_disparo : MonoBehaviour {
+
+	public GameObject bala;
+	public Transform puntoDisparo;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+//			Debug.Log("HOLAAAA");
+			var clone = Instantiate (bala, puntoDisparo.position,
+			                         Quaternion.identity) as GameObject;
+		}
+	}
+}
+
---
+++ Assets/Scripts/hurtSystem.cs
@@ -0,0 +1,47 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+
+public class hurtSystem : MonoBehaviour {
+	public string tagDamage = "Default";
+	public int damage = 5;
+	public float force = 0;
+	public bool destroyOnCollision = false;
+	public bool destroyOnTrigger = true;
+	
+	private LifeSystem ds;
+	
+	void OnCollisionStay2D(Collision2D target){
+		
+		herir (target.transform);
+		
+		if (destroyOnCollision) {
+			Destroy(gameObject);
+		}
+	}
+	
+	void OnTriggerStay2D(Collider2D target){
+		
+		herir (target.transform);
+	}
+	
+	void herir(Transform target){
+		if (target.transform.tag == tagDamage) {
+			Debug.Log (target.transform.tag);
+			ds = target.transform.GetComponent<LifeSystem>();
+			if(ds){
+				ds.hurt(damage);
+				if(force > 0){
+					var direction = target.position - transform.position  ;
+					ds.rigidbody2D.AddForce(direction.normalized * force/10,ForceMode2D.Impulse);
+				}
+			}
+			if (destroyOnTrigger) {
+				Destroy(gameObject);
+			}
+		}
+		
+		
+	}
+	
+	
+}
